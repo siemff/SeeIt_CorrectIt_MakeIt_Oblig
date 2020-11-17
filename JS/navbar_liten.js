b856e5ca65ba3ece3dcc519032d5328591ab2000
siemff <71024108+siemff@users.noreply.github.com>
2020-11-17 15:43:12 +0100

    Responsiv endringer

---
+++ JS/navbar_liten.js
@@ -0,0 +1,8 @@
+function menuFunction() {
+    var i = document.getElementById("liten_nav_links");
+    if (i.style.display === "block") {
+        i.style.display = "none";
+    } else {
+        i.style.display = "block";
+    }
+}
